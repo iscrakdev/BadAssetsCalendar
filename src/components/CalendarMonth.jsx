a08a64a94b457a946fbc0a528275ee240139d1c4
Emma Borth <emma.borth.fec3@statefarm.com>
2023-12-14 13:02:48 -0600

    added CalendarMonth component

---
+++ src/components/CalendarMonth.jsx
@@ -0,0 +1,19 @@
+const CalendarMonth = () => {
+    return (
+        <div className = "month-calendar-container">
+            <div class = "calendar-table">
+                <table>
+                    <tr>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    )
+}
