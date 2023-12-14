28a31c33e8560d868268653b13c658e1fc25c622
Emma Borth <emma.borth.fec3@statefarm.com>
2023-12-14 12:57:26 -0600

    added Navigation component - need router for it

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+const Navigation = () => {
+  return (
+    <div className="navigation-container">
+      <h1>World's Best Planning App</h1>
+      <div className="navigation-links">
+        <p>Calendar</p>
+        <p>Events</p>
+        <p>Notes</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
