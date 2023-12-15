818a138946487dcf371506bb8cd19ca786993417
Isaac Rakstad <iscrakdev@gmail.com>
2023-12-15 12:12:50 -0600

    added check for if holidays are in local storage, it will pull from LS otherwise it will pull from API and update LS

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import Navigation from "./components/Navigation";
 import CalendarMonth from "./components/CalendarMonthView/CalendarMonth.jsx";
 import getCalendar from "./util/getCalendar.js";
 import getHolidaysByYear from "./util/getHolidaysFromAPI.js";
+import { useEffect, useState } from "react";
 
 function App() {
-  const yearArr = getCalendar(2023)
-  const holidays = getHolidaysByYear(2023)
-  const curMonth = new Date().getMonth()
+  const [holidays, setHolidays] = useState([]);
+  const yearArr = getCalendar(2023);
+  const curMonth = new Date().getMonth();
+
+  useEffect(() => {
+    // Checks if we have Holidays in local storage
+    if (localStorage.getItem("2023Holidays") !== null) {
+      // If we do we get them from local storage
+      setHolidays(JSON.parse(localStorage.getItem("2023Holidays")));
+    } else {
+      // If not we fetch them and update local storage
+      getHolidaysByYear(2023).then((data) => {
+        setHolidays(data);
+        localStorage.setItem("2023Holidays", JSON.stringify(data));
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
       <Navigation navigation={Navigation}></Navigation>
-      <CalendarMonth month={yearArr[curMonth]}/>
+      <CalendarMonth month={yearArr[curMonth]} holidays={holidays} />
     </div>
   );
 }
@@ -20,39 +36,6 @@ export default App;
 
 /* 
 Play-Ground:
-
-let numOfDays31 = [ 'Jan', 'Mar', 'May', 'Jul', 'Aug', 'Oct', 'Dec' ]
-let numOfDays30 = [ 'Apr', 'June', 'Sep', 'Nov' ]
-month = 31, 30, 29
-
-let monthArr = []
-while (month > 0) {
-  let week = []
-  let startDay = new Date('2023-12-1').getDay() // 5 
-  
-  if (monthArr.length === 0) {
-    for (let i = 0; i < startDay; i++) {
-      week.push(null)
-    } 
-  }
-  
-  for (let i = week.length; i < 7; i++) {
-    week.push(<CalendarDay date={'theDate'}>)
-  }  
-
-  monthArr.push(week)
-}
-
-let startDay = new Date('2023-12-1').getDay() // 5 
-for (let i = 0; i < startDay; i++) {
-  week.push(null)
-}
-
-for (let i = week.length; i < 7; i++) {
-  week.push(<CalendarDay date={'theDate'}>)
-}
-
-let weekdays = ['sunday', 'monday','tuesday','monday','monday','monday','Saturday',]
 
 let date = '2023-12-1'
 let [year, month, day] = date.split('-')
@@ -72,15 +55,6 @@ let month = [
   week,
   week
 ]
-
-[undefined, undefined, undefined, undefined, undefined, {day}, {day}]
-
-for (let day of week) {
-  if (day === undefined) {return ""}
-} else {
-  return <CalendarDay />
-}
-
 
 29 30 31 tds []
 for (let i = 0; i <= 30; i++) {
--- src/util/getHolidaysFromAPI.js
+++ src/util/getHolidaysFromAPI.js
@@ -1,12 +1,15 @@
-const getHolidaysByYear = (year) => {
-  return fetch(
+const getHolidaysByYear = async (year) => {
+  const response = await fetch(
     `https://api.api-ninjas.com/v1/holidays?country=US&year=${year}`,
     {
       method: "GET",
       headers: { "X-Api-Key": "PF3n7BIaSlGjQaBmfasuww==iui3kGA7br0VXfbQ" },
       contentType: "application/json",
     }
-  ).then((response) => response.json());
+  )
+
+  const data = await response.json()
+  return data  
 };
 
 export default getHolidaysByYear;
